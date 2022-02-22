ab6b2ae8f1dec2240410291be8608df202ac30f6
tam-sal <t.saleh.gc@gmail.com>
2022-02-22 03:09:11 -0300

    "commit on master"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/layout/Header'
+import Nav from './components/layout/Nav'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
---
+++ src/components/layout/Nav.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Nav = (props) => {
+    return (
+        <div>Nav</div>);
+}
+export default Nav;
