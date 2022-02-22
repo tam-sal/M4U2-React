efb3583caa9af0647b353406da222e1f0ac0ae50
tam-sal <t.saleh.gc@gmail.com>
2022-02-22 03:26:48 -0300

    "first npm call successful"

--- src/App.js
+++ src/App.js
@@ -7,20 +7,11 @@ import Footer from './components/layout/Footer'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Header/>
+      <Nav/>
+      <Footer/>
+      
     </div>
   );
 }
