ca671637d2cf83a981cf1564b64c019556ef124e
tam-sal <t.saleh.gc@gmail.com>
2022-03-30 21:55:10 -0300

    "Back-To-Top" successfully added

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => {
     <footer>
       <p>Diseñada por Tamer Saleh - &copy;2022</p>
         <div className="back-top" title="ir arriba">
-        <a href="#top"><i className="fa fa-angle-up"></i></a>   
+        <a href="#" onClick={(e) => {e.preventDefault();window.scrollTo(0,0)}}><i className="fa fa-angle-up"></i></a>
       </div>
     </footer>);
 }
